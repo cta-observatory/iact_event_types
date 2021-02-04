a6d874eb7ce845c756edadf77e27b7a87826c2a2
Orel Gueta <22728856+orelgueta@users.noreply.github.com>
2021-02-04 21:02:32 +0100

    Restructure to allow studying test dataset size effect. Add option to use previously reduced data. Other small improvements.

--- scripts/control_plots.py
+++ scripts/control_plots.py
@@ -12,11 +12,19 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    dl2_file_name = (
-        '/lustre/fs21/group/cta/users/maierg/analysis/AnalysisData/uploadDL2/'
-        'Paranal_20deg/gamma_onSource.S.3HB9-FD_ID0.eff-0.root'
-    )
-    dtf = event_types.extract_df_from_dl2(dl2_file_name)
+    start_from_DL2 = False
+    if start_from_DL2:
+        # Prod5
+        dl2_file_name = (
+            '/lustre/fs22/group/cta/users/maierg/analysis/AnalysisData/'
+            'prod5-Paranal-20deg-sq08-LL/EffectiveAreas/'
+            'EffectiveArea-50h-ID0-NIM2LST2MST2SST2SCMST2-g20210921-V3/BDT.DL2.50h-V3.g20210921/'
+            'gamma_onSource.S.BL-4LSTs25MSTs70SSTs-MSTF_ID0.eff-0.root'
+        )
+        dtf = event_types.extract_df_from_dl2(dl2_file_name)
+    else:
+        dtf = event_types.load_dtf()
+
     dtf_e = event_types.bin_data_in_energy(dtf)
 
     labels, train_features = event_types.nominal_labels_train_features()
