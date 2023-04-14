8e3925f69864f66c1e9c56556ef3d04f0d7f8947
JBernete <jbernete@ucm.es>
2023-04-14 18:01:44 +0200

    Allow reducing onSource root files for electrons and protons.

--- scripts/reduce_data.py
+++ scripts/reduce_data.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         # La Palma
         "../../data/LongFiles/North/"
     )
-    particles = ["gamma_onSource", "gamma_cone", "electron", "proton"]
+    particles = ["gamma_onSource", "electron_onSource", "proton_onSource", "gamma_cone", "electron", "proton"]
     bins_off_axis_angle = [0, 1, 2, 3, 4, 5]
     layout_desc = "N.D25-4LSTs09MSTs-MSTN"  # LaPalma
     # layout_desc = 'S-M6C8aj-14MSTs37SSTs-MSTF'  # Paranal
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     dl2_file_list = list()
     for particle in particles:
         for off_axis_bin in bins_off_axis_angle:
-            if off_axis_bin > 0 and particle == "gamma_onSource":
+            if off_axis_bin > 0 and particle in ["gamma_onSource", "electron_onSource", "proton_onSource"]:
                 continue
             dl2_file_list.append(f"{particle}.{layout_desc}_ID0.eff-{off_axis_bin}.root")
 
